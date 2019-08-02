9ff008e02a3aca67ee0589dd9c853a2aff366599
Thiago Lago <thiagolagoo2@gmail.com>
2019-08-02 17:17:11 -0300

    :sparkles: [ADD] Reactotron

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import './config/ReactotronConfig';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  }
+    textAlign: 'center',
+    margin: 10,
+  },
 });
 
 export default function App() {
